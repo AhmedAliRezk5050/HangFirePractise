521b95079641847650a9a7a8504ade81226a4f9b
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-12-05 17:17:42 +0200

    Edits

--- HangFirePractise.Web/Program.cs
+++ HangFirePractise.Web/Program.cs
@@ -1,10 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Hangfire;
+using Hangfire.Storage.SQLite;
 
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
 
-builder.Services.AddHangFire();
+builder.Services.AddHangfire(config =>
+{
+    config.UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseSQLiteStorage(configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -12,8 +20,12 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseHangfireDashboard();
+app.MapHangfireDashboard();
 
 app.Run();
