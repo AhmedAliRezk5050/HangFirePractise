7a756465fbcb3654e484ef0caf2545588ef20021
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-12-05 19:19:54 +0200

    Add Drivers Controller

---
+++ HangFirePractise.Web/Controllers/DriversController.cs
@@ -0,0 +1,61 @@
+using HangFirePractise.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangFirePractise.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DriversController : ControllerBase
+    {
+        // Create in-memory database
+        private static List<Driver> _drivers = new ();
+
+        private readonly ILogger<DriversController> _logger;
+
+        public DriversController(ILogger<DriversController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult AddDriver(Driver driver)
+        {
+            if (ModelState.IsValid)
+            {
+                _drivers.Add(driver);
+
+                return CreatedAtAction("GetDriver", new {driver.Id}, driver);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpGet]
+        public IActionResult GetDriver(Guid id)
+        {
+            var driver = _drivers.FirstOrDefault(d => d.Id == id);
+
+            if (driver is  null)
+            {
+                return NotFound();
+            }
+
+            return Ok(driver);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteDriver(Guid id)
+        {
+            var driver = _drivers.FirstOrDefault(d => d.Id == id);
+
+            if (driver is  null)
+            {
+                return NotFound();
+            }
+
+            driver.Status = 0;
+
+            return NoContent();
+        }
+    }
+}
